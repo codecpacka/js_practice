08bfa83fa6413dd6e0af43e7e06e0746e6058c2e
codecpacka <ramprasad.r2600@gmail.com>
2022-02-11 12:17:07 +0530

    array methods completed

---
+++ iterables.js
@@ -0,0 +1,12 @@
+console.log("iterables");
+//jismein hum loop laga sakte hain
+// note: string,array are iterable
+// example:
+const name="ramprasad";
+for(char of name){
+    console.log(char);
+}
+//array like object 
+//jinke pass length property hoti hain
+//aur jinko hum index se acces kar sakte hain 
+//example stringb
